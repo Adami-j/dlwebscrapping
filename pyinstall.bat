734ded86b2c20531bbb69e5130a41fc9ebc1e421
juju <adamij@3il.fr>
2023-04-18 00:13:28 +0200

    resolving utf8 glitch

--- pyinstall.bat
+++ pyinstall.bat
@@ -1,23 +1,23 @@
 @echo off
 
-rem Vérifier si Python est déjà installé
+rem Verifier si Python est deja installe
 python --version >nul 2>&1
 if %errorlevel% == 0 (
     echo Python est deja installe
 ) else (
-    echo Python n'est pas installé, téléchargement de la dernière version...
+    echo Python n'est pas installe, telechargement de la derniere version...
     curl https://www.python.org/ftp/python/3.10.0/python-3.10.0-amd64.exe -o python_installer.exe
     echo Installation de Python...
     start /wait python_installer.exe /quiet InstallAllUsers=1 PrependPath=1
     del python_installer.exe
 )
 
-rem Vérifier si Pip est déjà installé
+rem Verifier si Pip est deja installe
 pip --version >nul 2>&1
 if %errorlevel% == 0 (
     echo Pip est deja installe
 ) else (
-    echo Pip n'est pas installé, téléchargement de la dernière version...
+    echo Pip n'est pas installe, telechargement de la derniere version...
     curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
     echo Installation de Pip...
     python get-pip.py
--- requirementInstall.bat
+++ requirementInstall.bat
@@ -1,18 +1,18 @@
 @echo off
 
-rem Vérifier si pip est installé
+rem Verifier si pip est installe
 pip --version >nul 2>&1
 if %errorlevel% == 0 (
-    echo pip est déjà installé
+    echo pip est deja installe
 ) else (
-    echo pip n'est pas installé, téléchargement de la dernière version...
+    echo pip n'est pas installe, telechargement de la derniere version...
     curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
     echo Installation de pip...
     python get-pip.py
     del get-pip.py
 )
 
-rem Installer les bibliothèques requises
+rem Installer les bibliotheques requises
 pip install tkinter
 pip install requests
 pip install beautifulsoup4
@@ -20,4 +20,4 @@ pip install undetected-chromedriver
 pip install selenium
 pip install urllib3
 
-echo Terminé.
+echo Termine.
